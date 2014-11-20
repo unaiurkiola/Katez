790a0ccddbab73942333b0a55c2f8f0fa0050c5b
unaiurkiola <unaiurkiola@gmail.com>
2014-11-20 11:43:34 +0100

    katez

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 Katez
-=====
-
-
-### Musika taldearentzat web orrialdea
+Musika taldearentzat web orrialdea
 
 http://www.riseagainst.com/
 
 Eredu bat
-
-
--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 // form
 // http://expressjs.com/4x/api.html#req.params
+
 var express = require('express');
 var app = express();
+
+
+var sqlze = require('sequelize');
+var mysql =  require('mysql');
+// var db = new sqlze('databasename', 'username', 'password',{
+var db = new sqlze('katez', 'root', 'unai',{
+dialect: 'mysql',
+port: 3306
+});
+//
+db
+.authenticate()
+.complete(function(err){
+if(!!err) {
+console.log('Unable to connect to database: ', err);
+} else {
+console.log('Connection OK!');
+}
+});
+
+
 
 // body-parser for POST
 // https://github.com/expressjs/body-parser
@@ -18,11 +40,40 @@ app.use(express.static(__dirname + '/public'));
 // **********************************************
 
 app.get('/', function(req, res) {
-    res.redirect('/hello.html');
+    res.redirect('/index.html');
 });
+
+
+
+
+
+app.get('/partaideak', function(req, res) {
+db.query('select * from argazkiak').success(function(rows){
+// no errors
+  console.log(rows);
+  res.json(rows);
+// res.json(JSON.stringify(rows));
+});
+});
+
+app.get('/bideoak', function(req, res) {
+db.query('select * from bideoak').success(function(rows){
+// no errors
+  console.log(rows);
+  res.json(rows);
+// res.json(JSON.stringify(rows));
+});
+});
+
 
 
 
 var server = app.listen(process.env.PORT || 3000, function(){
     console.log('Listening in port %d', server.address().port);
 });
+
+
+  //connection.end(function(err){
+//Cerramos la conexion con nuestro mysql.
+//});
+
